6b9820542bae403ea9c8840174c1ec570b67c74f
nicholasladika <129184657+nicholasladika@users.noreply.github.com>
2024-08-20 08:39:23 -0300

    jghngh

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 const elemento = document.querySelector(".teste");
+
+const elemento = document.querySelector(".teste2");
+
+titulo.textContent = "olá mundo";
+
+
+let contador = 1 ;
+
+let contador = [1,2,5,8,9];
+
+let perguntas = {
+
+    
+}
