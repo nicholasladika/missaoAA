a78aef316238b2c71ccb6eeb7bf920f9e9300f0a
nicholasladika <129184657+nicholasladika@users.noreply.github.com>
2024-08-27 08:19:41 -0300

    eeeee

--- main.js
+++ main.js
@@ -1,15 +1,5 @@
-const elemento = document.querySelector(".teste");
-
-const elemento = document.querySelector(".teste2");
-
-titulo.textContent = "olá mundo";
-
-
-let contador = 1 ;
-
-let contador = [1,2,5,8,9];
-
-let perguntas = {
-
-    
-}
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPergunta = document.querySelector(".caixa-perguntas");
+const caixaAlernativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resulatdo");
+const textoResulatdo = document.querySelector(".texto-resultado");
