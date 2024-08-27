58a4606d428e3f7745937cd852bb063ab0134fb8
nicholasladika <129184657+nicholasladika@users.noreply.github.com>
2024-08-27 08:38:54 -0300

    jjjkk

--- main.js
+++ main.js
@@ -3,3 +3,5 @@ const caixaPergunta = document.querySelector(".caixa-perguntas");
 const caixaAlernativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resulatdo");
 const textoResulatdo = document.querySelector(".texto-resultado");
+
+
